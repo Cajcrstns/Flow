31facd1f0e1260a96a2775268a923a40b1d67799
Cobe Carstens <cobecarstens@Cobes-MacBook-Air.local>
2024-06-13 15:21:11 +0200

    p

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Routes>
       <Route path="/*" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/rooms" element={<Works />} />
+      <Route path="/mindfullness" element={<Blog />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/two-person-room" element={<SampleProject2 />} />
+      <Route path="/four-person-room" element={<SampleProject4 />} />
+      <Route path="/six-person-room" element={<SampleProject6 />} />
     </Routes>
   </Router>
 );
