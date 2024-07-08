065ce57621a3b5ad1dd3a107e7312cbbad4e495a
Cobe Carstens <cobecarstens@Cobes-MacBook-Air.local>
2024-07-08 18:50:58 +0200

    p

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger  from 'gsap/dist/ScrollTrigger';
+import Scrollbar from 'smooth-scrollbar'
+
 
 import Transition from '../../components/transition/Transition';
 import View from "../../components/view/View";
@@ -14,33 +16,55 @@ gsap.registerPlugin(ScrollTrigger)
 import "./home.css";
 
 function Home() {
-useEffect(() => {
-    const sectionColor = document.querySelectorAll('[data-bgcolor]');
-    sectionColor.forEach((colorSection, i) => {
-      const prevBgColor = i === 0 ? '' : sectionColor[i - 1].dataset.bgcolor;
-      const prevTextColor =
-        i === 0 ? '' : sectionColor[i - 1].dataset.textcolor;
-
-      ScrollTrigger.create({
-        trigger: colorSection,
-        start: 'top 50%',
-        onEnter: () =>
-          gsap.to('.main', {
-            backgroundColor: colorSection.dataset.bgcolor,
-            color: colorSection.dataset.textcolor,
-            overwrite: 'auto',
-        }),
-        onLeaveBack: () =>
-          gsap.to('.main', {
-            backgroundColor: prevBgColor,
-            color: prevTextColor,
-            overwrite: 'auto',
-          }),
-      });
-    });
-
-    return () => {};
-}, []); 
+    useEffect(() => {
+        const scrollBar = Scrollbar.init(document.querySelector('.main'), {
+          damping: 0.06,
+          delegateTo: document,
+          alwaysShowTracks: false,
+          speed: 3,
+        });
+        ScrollTrigger.defaults({
+          scroller: '.main',
+        });
+        ScrollTrigger.scrollerProxy('.main', {
+          scrollTop(value) {
+            if (arguments.length) {
+              scrollBar.scrollTop = value;
+            }
+            return scrollBar.scrollTop;
+          },
+        });
+    
+        scrollBar.addListener(ScrollTrigger.update);
+    
+        const sectionColor = document.querySelectorAll('[data-bgcolor]');
+        sectionColor.forEach((colorSection, i) => {
+          const prevBgColor = i === 0 ? '' : sectionColor[i - 1].dataset.bgcolor;
+          const prevTextColor =
+            i === 0 ? '' : sectionColor[i - 1].dataset.textcolor;
+    
+          ScrollTrigger.create({
+            trigger: colorSection,
+            scroller: '.main',
+            start: 'top 50%',
+            onEnter: () =>
+              gsap.to('.main', {
+                backgroundColor: colorSection.dataset.bgcolor,
+                color: colorSection.dataset.textcolor,
+                overwrite: 'auto',
+            }),
+            onLeaveBack: () =>
+              gsap.to('.main', {
+                backgroundColor: prevBgColor,
+                color: prevTextColor,
+                overwrite: 'auto',
+              }),
+          });
+        });
+    
+        return () => {};
+    }, []); 
+    
   return (
     <div className='main'>
         <div className='bgcolor'></div>
